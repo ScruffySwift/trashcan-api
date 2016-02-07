529ffeb8a11ed435394251ace08b3099726e0eb8
cfrancis <connor.k.francis@jpl.nasa.gov>
2016-02-07 01:10:48 -0800

    alexa update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
+var AWS = require('aws-sdk');
 
+// Set region to govcloud
+AWS.config.update({region: 'us-east-1'});
+var s3 = new AWS.S3();
 var PUBLIC_PATH = process.env.PUBLIC_PATH || 'public';
 
 var trashStatus = {};
@@ -21,7 +25,12 @@ app.post('/api/full/:trashcan', function(req, res) {
 
 app.get('/api', function(req, res) {
   var results = [];
+  var alexaUpdate = "These trash cans are full ";
+  alexaUpdate = JSON.stringify(alexaUpdate);
   for (prop in trashStatus) {
+    if(trashStatus[prop] == "full"){
+      alexaUpdate += prop;
+    }
     if (trashStatus.hasOwnProperty(prop)) {
       results.push({
         name: prop,
@@ -29,6 +38,19 @@ app.get('/api', function(req, res) {
       });
     }
   }
+  var params = {
+    Key: 'text.txt',
+    Bucket: 'testinghackathoniot',
+    Body : alexaUpdate
+  }; 
+  s3.putObject(params, function(err, data) {
+    if (err) {
+      console.log(err);
+      //res.status(500).send('Could not save data to s3');
+    } else {
+        console.log('success');
+      }
+  });
   res.send(JSON.stringify(results));
 });
 
