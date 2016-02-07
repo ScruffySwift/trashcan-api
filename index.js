81991f5920f45d312db5b69de31a0c20388305ce
Stefan Eng <stefaneng13@gmail.com>
2016-02-06 17:29:58 -0800

    Info route for trash can status

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 var express = require('express');
 var app = express();
 
+var trashStatus = {};
+
 app.post('/empty/:trashcan', function(req, res) {
   var trashcanName = req.params.trashcan;
+  trashStatus[trashcanName] = 'empty';
   console.log(trashcanName, 'emptied');
+  res.send('OK');
 });
 
 app.post('/full/:trashcan', function(req, res) {
   var trashcanName = req.params.trashcan;
+  trashStatus[trashcanName] = 'full';
   console.log(trashcanName, 'full');
   res.send('OK');
 });
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/info', function(req, res) {
+  res.send(JSON.stringify(trashStatus));
 });
 
 app.listen(3000, function () {
