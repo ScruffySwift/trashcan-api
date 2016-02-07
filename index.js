b3c485c80acb11267f7ede007e4ddb8fbf521444
Stefan Eng <stefaneng13@gmail.com>
2016-02-06 16:13:25 -0800

    Post route for trashcans to update

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.post('/update/:trashcan', function(req, res) {
+  var trashcanName = req.params.trashcan;
+  console.log(trashcanName);
+  res.send('OK');
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
