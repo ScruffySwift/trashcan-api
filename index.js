bdc4aa810605d8c00ad152b194a392cf8785396c
Stefan Eng <stefaneng13@gmail.com>
2016-02-06 18:36:26 -0800

    Serve static content from PUBLIC_PATH or 'public'

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 var express = require('express');
 var app = express();
 
+var PUBLIC_PATH = process.env.PUBLIC_PATH || 'public';
+
 var trashStatus = {};
 
-app.post('/empty/:trashcan', function(req, res) {
+app.post('/api/empty/:trashcan', function(req, res) {
   var trashcanName = req.params.trashcan;
   trashStatus[trashcanName] = 'empty';
   console.log(trashcanName, 'emptied');
   res.send('OK');
 });
 
-app.post('/full/:trashcan', function(req, res) {
+app.post('/api/full/:trashcan', function(req, res) {
   var trashcanName = req.params.trashcan;
   trashStatus[trashcanName] = 'full';
   console.log(trashcanName, 'full');
   res.send('OK');
 });
 
-app.get('/', function(req, res) {
+app.get('/api', function(req, res) {
   res.send(JSON.stringify(trashStatus));
 });
+
+app.use(express.static(PUBLIC_PATH));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
